900855ef44b86d9eac1f5676fff3c6fd9f00ae78
VickyVis <114154876+VickyVis@users.noreply.github.com>
2024-09-30 17:55:32 +0530

    registration commit

---
+++ lib/supabase.ts
@@ -0,0 +1,15 @@
+"use server"
+
+import { createClient } from "@supabase/supabase-js"
+
+
+
+export async function createSupabaseAdmin() {
+	return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.SUPABASE_SERVICE_ROLE!, {
+        auth: {
+            autoRefreshToken: false,
+            persistSession: false
+        },
+    });
+}
+
---
+++ store.ts
@@ -0,0 +1,73 @@
+import { create } from 'zustand';
+import { cloneDeep } from 'lodash';
+
+type State = {
+  userSession: object;
+  profileDetails: object;
+  containers: Array<object>
+  snackbar: {
+    open: boolean;
+    severity: string;
+    msg: string;
+  };
+
+  registrationForm: {
+    fullName: string;
+    dob: string;
+    phoneNumber: string;
+    email: string;
+    password: string;
+  };
+};
+
+const initState: State = {
+  userSession: {},
+  profileDetails: {},
+  snackbar: {
+    open: false,
+    severity: '',
+    msg: '',
+  },
+
+  registrationForm: {
+    fullName: '',
+    dob: '',
+    phoneNumber: '',
+    email: '',
+    password: '',
+  },
+};
+
+type Action = {
+  openSnackbar: (severity: string, msg: string) => void;
+  closeSnackbar: () => void;
+
+  setProfileDetailsAndSession: (session: State['userSession'], profileDetails: State['profileDetails']) => void
+
+  setRegistrationFormField: (key: string, value: string) => void;
+  resetRegistrationForm: () => void;
+
+  setUserSession: (session: State['userSession']) => void;
+  clearUserSession: () => void;
+
+  setContainers: (containers: State['containers']) => void
+};
+
+export const useStore = create<State & Action>((set) => ({
+  ...cloneDeep(initState),
+
+  openSnackbar: (severity, msg) => set((state) => ({...state, snackbar: { open: true, severity, msg } })),
+  closeSnackbar: () => set((state) => ({snackbar: { ...state.snackbar, open: false}})),
+
+  setProfileDetailsAndSession: (userSession, profileDetails) => set(() => ({userSession, profileDetails})),
+
+  setRegistrationFormField: (key, value) => set((state) => ({
+    registrationForm: { ...state.registrationForm, [key]: value },
+  })),
+  resetRegistrationForm: () => set(() => cloneDeep(initState)), // I modify this check it later
+
+  setUserSession: (session) => set(() => ({ userSession: session})),
+  clearUserSession: () => set(() => ({ userSession: {} })),
+
+  setContainers: (containers) => set(() => ({containers})),
+}));
